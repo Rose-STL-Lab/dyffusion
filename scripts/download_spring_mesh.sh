6e212c424a09771e5ea1d3132c5415279037bcec
Salva Rühling Cachay <43278225+salvaRC@users.noreply.github.com>
2024-02-13 10:25:01 -0800

    Update download_spring_mesh.sh
    Different approach of supporting running the script from root dir

--- scripts/download_spring_mesh.sh
+++ scripts/download_spring_mesh.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
-bash scripts/download_physical_systems_data.sh full spring-mesh
+# Change the directory to the script's directory, if it is not already
+cd "$(dirname "$0")"
+
+# Download the spring-mesh data
+bash download_physical_systems_data.sh full spring-mesh
 sleep 10
