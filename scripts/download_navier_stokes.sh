a48607bde6165495a09bd2b8dcf428f7077b2d20
Salva Rühling Cachay <43278225+salvaRC@users.noreply.github.com>
2024-02-13 10:24:07 -0800

    Update download_navier_stokes.sh

--- scripts/download_navier_stokes.sh
+++ scripts/download_navier_stokes.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
-bash scripts/download_physical_systems_data.sh full navier-stokes-multi
+# Change the directory to the script's directory, if it is not already
+cd "$(dirname "$0")"
+
+# Download the navier-stokes data
+bash download_physical_systems_data.sh full navier-stokes-multi
 sleep 5
